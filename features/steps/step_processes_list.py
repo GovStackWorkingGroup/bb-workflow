5290d59fe6493967508e1456605e1319b77419cd
aarelaponin <aare@fiscaladmin.com>
2022-09-03 22:21:50 +0300

    init step

--- features/steps/step_processes_list.py
+++ features/steps/step_processes_list.py
@@ -1,18 +1,18 @@
 from behave import *
 from hamcrest import assert_that, equal_to, is_not
 
-HOST = 'http://localhost/api/'
+HOST = 'http://localhost/'
 
 
-class DwSource(object):
+class Process(object):
 
     def __init__(self, endpoint_name):
         self._endpoint_name = endpoint_name
-        self._url = None
+        self._url = f'{HOST}{endpoint_name}'
 
     def count_of_records(self):
         if self._endpoint_name is not None:
-            self._url = f'{HOST}'
+            return 1
 
 
 @given('we have token from login')
@@ -22,10 +22,12 @@ def step_impl(context):
 
 @when('we request list from {endpoint_name} endpoint')
 def step_impl(context, endpoint_name):
-    context.dw_source = DwSource(endpoint_name)
+    context.obj = Process(endpoint_name)
     assert True is not False
 
 
 @then('we receive list of processes having {predefined_number} of processes')
 def step_impl(context, predefined_number):
+    count = context.obj.count_of_records()
+    assert count == 2
     assert context.failed is False
