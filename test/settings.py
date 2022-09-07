c500d7f7453142849032590feb50034c296a3837
Taylor Downs <taylor@openfn.org>
2022-09-07 13:20:51 +0100

    add requirements

---
+++ test/settings.py
@@ -0,0 +1,12 @@
+DATA_DIR = 'data'
+
+DEFAULT_VALUES = {
+    'protocol': 'http',
+    'host': 'localhost',
+    'port': 8000,
+    'prefix': 'api',
+    'suffix': 'v1'
+}
+
+DROP_LAST_SLASH = True
+RESULT_IS_PLAIN_JSON = True
