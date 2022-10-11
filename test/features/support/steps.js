9874e534a587260e33d7a2712af15775d8745f28
Taylor Downs <taylor@openfn.org>
2022-10-11 13:33:20 +0100

    Update steps.js

--- test/features/support/steps.js
+++ test/features/support/steps.js
@@ -2,11 +2,10 @@ const assert = require('assert');
 const { When, Then } = require('@cucumber/cucumber');
 
 When('we request a list of processes', async function () {
-  this.whatIHeard = await fetch('https://jsonplaceholder.typicode.com/todos/');
+  this.whatIHeard = await fetch('localhost/api/processes');
 });
 
 Then('we receive {int} processes', async function (expectedResponse) {
-  console.log('yay!');
   const data = await this.whatIHeard.json();
   assert.equal(data.length, expectedResponse);
 });
