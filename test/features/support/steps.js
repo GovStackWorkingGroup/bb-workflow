ab5df04727939bca70ae9f1b1cb2797f2c7f49b7
Farai Mutero <fmutero@gmail.com>
2022-11-16 14:00:11 +0200

    Camunda Tests

--- test/features/support/steps.js
+++ test/features/support/steps.js
@@ -8,12 +8,59 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 When('we request a list of processes from the api', async function () {
   this.whatIHeard = await fetch('https://localhost/processes');
-});
+})
 
 Then('we receive {int} processes', async function (expectedResponse) {
   const resp = this.whatIHeard;
-  assert.equal(resp.status, 201);
+  assert.equal(resp.status, 200);
 
   const data = await resp.json();
   assert.equal(data.length, expectedResponse);
 });
+
+When('we request a list of instances from the api', async function () {
+  this.whatIHeard = await fetch('https://localhost/instances');
+})
+
+Then('we receive status code {int} for the instances', async function (expectedResponse) {
+  const resp = this.whatIHeard;
+  assert.equal(resp.status, 200);
+
+  //const data = await resp.json();
+  //assert.equal(data.length, expectedResponse);
+});
+
+When('We request the details of an existing workflow processes from the api', async function () {
+  this.whatIHeard = await fetch('https://localhost/processes/ReviewInvoice:1:bf20fe53-5548-11ed-9dd4-0242ac150002');
+})
+
+Then('we receive status code {int} for the workflow process', async function (expectedResponse) {
+  const resp = this.whatIHeard;
+  assert.equal(resp.status, expectedResponse);
+});
+
+When('We request the details of an existing workflow processes instance from the api', async function () {
+  this.whatIHeard = await fetch('https://localhost/instances/c0717272-5548-11ed-9dd4-0242ac150002');
+})
+
+Then('we receive status code {int} for the workflow process instance', async function (expectedResponse) {
+  const resp = this.whatIHeard;
+  assert.equal(resp.status, expectedResponse);
+});
+
+When('We request to start a workflow process from the api', async function () {
+  let payload = JSON.stringify({"name": "Test"})
+  this.whatIHeard = await fetch('https://localhost/processes/ReviewInvoice:1:bf20fe53-5548-11ed-9dd4-0242ac150002/start', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: payload
+  });
+})
+
+Then('we receive status code 200 to confirm success', async function (expectedResponse) {
+  const resp = this.whatIHeard;
+  assert.equal(resp.status, expectedResponse);
+});
+
