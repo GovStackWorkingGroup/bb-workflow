3f0df801c981e2fe9bdd0a67496fc467eb7999da
Farai Mutero <fmutero@gmail.com>
2023-01-25 14:59:42 +0200

    unfished work on tests

--- test/features/support/steps.js
+++ test/features/support/steps.js
@@ -1,22 +1,45 @@
 const assert = require('assert');
-const { When, Then } = require('@cucumber/cucumber');
+const util = require('../../features/support/util.js')
+const endpoints = require('../../features/support/endpoints.json')
+const readYamlFile = require('read-yaml-file')
+const { When, Then } = require('@cucumber/cucumber')
+
 
 // TODO: is this the best way to allow insecure ssl?
 // TODO: and do we want to?
+const urlBase = "https://localhost"
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 When('we request a list of processes from the api', async function () {
-  this.whatIHeard = await fetch('https://localhost/processes');
+  let queryUrl = urlBase + endpoints.props[endpoints.processList].uri
+  console.log('QueryUrl : ' + queryUrl)
+  
+  this.whatIHeard = await fetch(queryUrl)
 })
 
 Then('we receive', async function () {
+
+  
+  let yamlData;
   const resp = this.whatIHeard;
-  assert.equal(resp.status, 200);
+
+  //1) Fetch OpenAPI spec
+
+  //2) Parse OpenAPI YAML
+  await readYamlFile('../test/features/support/swagger.yaml').then(data => {
+    yamlData = data  
+
+    assert.equal(resp.status, 200)
+
+    //3) Fetch Component Reference
+    const expectedComponent = yamlData.paths[endpoints.props[endpoints.processList].uri].get.responses["200"].content["application/json"].schema.items["$ref"]
+    
+    console.log(JSON.stringify(expectedComponent))    
+  });
 
   const data = await resp.json();
   assert.notEqual(data.length, 0);
-  
 });
 
 When('we request a list of instances from the api', async function () {
@@ -55,11 +78,11 @@ Then('we receive status code {int} for the workflow process instance', async fun
 });
 
 When('We request to start a workflow process from the api', async function () {
-  let payload = JSON.stringify({"name": "Test"})
+  let payload = JSON.stringify({ "name": "Test" })
   let response = await fetch('https://localhost/processes')
   let processList = await response.json()
   let processQueryUrl = 'https://localhost/processes/' + processList[0].id + '/start';
-  
+
   this.whatIHeard = await fetch(processQueryUrl, {
     method: 'POST',
     headers: {
