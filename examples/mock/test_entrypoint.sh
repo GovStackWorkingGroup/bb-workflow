30064e40e6a3f3b57db0c1ac35d328ec27b39bd0
Taylor Downs <downs.taylor@gmail.com>
2023-03-24 13:48:20 +0000

    mock attempt and remove server spec from openapi

--- examples/mock/test_entrypoint.sh
+++ examples/mock/test_entrypoint.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
+# Make a copy of the API spec
+cp ../../api/workflow-engine.yaml ./api/workflow-engine.yaml
+
+# Start the docker compose/build process to serve the spec via Prism
 docker compose up -d
