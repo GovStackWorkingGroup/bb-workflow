9176d2774f705a19e4486ca2f2852baf9495b141
Taylor Downs <taylor@openfn.org>
2023-06-16 13:34:52 +0100

    return apiToken

--- examples/camunda/test_entrypoint.sh
+++ examples/camunda/test_entrypoint.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env bash
 
 cp .env.example .env && docker-compose up -d
+
+curl -X POST -d '{ name: "John Doe" }' http://localhost:4000/api/createPatient
+curl -X POST -d '{ date: "2023-03-12", patient: "John Doe" }' http://localhost:4000/api/createVisit
+
+token = curl -X POST -d '{ username: "admin", password: "welcome123" }' http://localhost:4000/api/login
+
+{ apiToken: token }
